a5ea8c1f4dae50e2f30ce5a4edeafc5c964f002c
mukuka-mulenga <mukukamulenga356@gmail.com>
2024-07-15 14:10:05 +0200

    latest

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ async def getDefault():
     return {
         'message':"Welcome to maidy hub zm backend!"
     }
+
+
+@app.get('/api/users',tags=['users'])
+async def getUsers():
+    return {
+        [
+            {'name':"Mukuka Mulenga",'program':"Diploma in computerr studies"}
+        ]
+    }
